71e091c7d3773bbc40b8b7af9a4ca2258bd5e3b0
Mustafa Ahmed <mostafafikry97@gmail.com>
2024-10-20 23:21:12 +0300

    initializing trie for better search for products

--- app/_lib/Trie.ts
+++ app/_lib/Trie.ts
@@ -1,1 +1,19 @@
-class Trie {}
+class TrieNode {
+  value: string;
+  wordEnd: boolean;
+  children: Map<string, null | TrieNode>;
+
+  constructor(value: string) {
+    this.value = value;
+    this.children = new Map();
+    this.wordEnd = false;
+  }
+}
+
+export class Trie {
+  root: TrieNode;
+
+  constructor() {
+    this.root = new TrieNode("");
+  }
+}
