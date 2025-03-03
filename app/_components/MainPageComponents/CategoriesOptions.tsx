52cf3c1a6456fdf2e688c3c20441ce8971a0576c
Mustafa Ahmed <mostafafikry97@gmail.com>
2025-03-03 18:58:22 +0200

    - Fethch categories in CategoriesOPtions section in the main page using the context of categories

--- app/_components/MainPageComponents/CategoriesOptions.tsx
+++ app/_components/MainPageComponents/CategoriesOptions.tsx
@@ -1,9 +1,10 @@
+"use client";
 import { useCategories } from "@/app/_context/CategoriesProvider";
 import { getCategories } from "@/app/_lib/APIs/categoriesAPIs";
 import Link from "next/link";
 
-async function CategoriesOptions() {
-  const categories = await getCategories();
+function CategoriesOptions() {
+  const { categories } = useCategories();
   return (
     <div className="sm:mb-0 mb-5">
       <ul className="flex items-center justify-center  list-none py-5 flex-wrap gap-3 sm:bg-white bg-slate-100  sm:gap-3 md:gap-16 sm:px-2 md:px-24 categoryList">
