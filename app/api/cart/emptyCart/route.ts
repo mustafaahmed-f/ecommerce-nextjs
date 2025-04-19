88c6e023da25a9c4819673c8998299ad9cf76e68
Mustafa Ahmed <mostafafikry97@gmail.com>
2025-04-19 22:30:38 +0200

    - [ Add ] => Empty cart API is finished and tested using postman

---
+++ app/api/cart/emptyCart/route.ts
@@ -0,0 +1,78 @@
+import { getUserId } from "@/app/_lib/getUserId";
+import { withMiddleWare } from "@/app/_lib/withMiddleWare";
+import cartModel from "@/app/_mongodb/models/cartModel";
+import productsModel from "@/app/_mongodb/models/productsModel";
+import { CartProduct } from "@/app/cart/_types/CartType";
+import mongoose from "mongoose";
+import { NextRequest, NextResponse } from "next/server";
+
+export const PATCH = withMiddleWare({
+  applyAuth: true,
+  middleWares: [],
+  handler: async (request: NextRequest) => {
+    //TODO : Re-use session inside the save method when convert to mongoDB atlas
+    //// Start session of mongoose so if one of the updating processes ( product or cart ) failed, both of the processes stop:
+    const session = await mongoose.startSession();
+    session.startTransaction();
+    try {
+      const searchParams = request.nextUrl.searchParams;
+      const userId = await getUserId();
+      //// Check if cart exists and belongs to user:
+      const cart = await cartModel.findOne({
+        userID: userId,
+        _id: searchParams.get("cartId"),
+      });
+      if (!cart) {
+        return NextResponse.json(
+          {
+            success: false,
+            error: "Cart is not found or cart doesn't belong to user",
+          },
+          { status: 404 },
+        );
+      }
+
+      //TODO : loop over products, Find each one and update its stock and save it
+      //TODO : try to find best practice for this and use parallel updating of products
+      //TODO : make the products array in cart empty ( [] ) and save cart
+
+      const missingProducts: number[] = [];
+
+      const updatedPromises = cart.products.map(async (p: CartProduct) => {
+        let product = await productsModel.findOne({ productId: p.productID });
+        if (product) {
+          product.stock += p.quantity;
+          return product.save();
+        } else {
+          missingProducts.push(p.productID);
+        }
+      });
+
+      if (missingProducts.length > 0) {
+        throw new Error(
+          `Products of ids : ${missingProducts.join(", ")} were not found or failed to update them !!`,
+          { cause: 400 },
+        );
+      }
+
+      await Promise.all(updatedPromises);
+
+      cart.products = [];
+      cart.subTotal = 0;
+      await cart.save();
+
+      await session.commitTransaction();
+      session.endSession();
+
+      return NextResponse.json({
+        success: true,
+        message: "Product has been removed successfully !!",
+        cart,
+      });
+    } catch (error: any) {
+      await session.abortTransaction();
+      session.endSession();
+      return NextResponse.json({ success: false, error: error?.message });
+    }
+  },
+});
