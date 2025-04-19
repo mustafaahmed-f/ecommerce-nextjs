7c45ac532de7a16d1be47b8e8a6088454d4f2467
Mustafa Ahmed <mostafafikry97@gmail.com>
2025-04-20 00:50:42 +0200

    - [ Modify ] => Reconfigured the redisClient.ts

--- app/_lib/redisClient.ts
+++ app/_lib/redisClient.ts
@@ -1,20 +1,4 @@
-// import { Redis } from "@upstash/redis";
+import { Redis } from "@upstash/redis";
 
-let redisClient: any = null;
-
-export async function getRedisClient() {
-  if (!redisClient) {
-    const { Redis } = await import("@upstash/redis");
-    redisClient = new Redis({
-      url: process.env.UPSTASH_REDIS_REST_URL,
-      token: process.env.UPSTASH_REDIS_REST_TOKEN,
-    });
-  }
-  return redisClient;
-}
-
-// Only initialize the Redis client when it's actually needed
-export async function useRedisClient() {
-  const redis = await getRedisClient();
-  return redis;
-}
+const redis = Redis.fromEnv(); // loads credentials from env vars
+export { redis };
