698af2d56fa4bc98a12c0a2a05af38794d99415e
Mustafa Ahmed <mostafafikry97@gmail.com>
2025-05-21 18:45:28 +0300

    - [ Add ] => installed shadcn to be used instead of mui material for smaller bundle size

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,8 @@
 import type { Config } from "tailwindcss";
+import tailwindcssAnimate from "tailwindcss-animate";
 
 const config: Config = {
+  darkMode: ["class"],
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -9,28 +11,69 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
         primary: {
-          50: "#E1E8EF",
-          100: "#D4DEE7",
-          200: "#B7C7D7",
-          300: "#99B0C7",
-          400: "#7C99B6",
-          500: "#5E82A6",
-          600: "#4C6B8A",
-          700: "#3C546C",
-          800: "#2C3D4F",
-          900: "#1B2631",
-          950: "#141C24",
+          "50": "#E1E8EF",
+          "100": "#D4DEE7",
+          "200": "#B7C7D7",
+          "300": "#99B0C7",
+          "400": "#7C99B6",
+          "500": "#5E82A6",
+          "600": "#4C6B8A",
+          "700": "#3C546C",
+          "800": "#2C3D4F",
+          "900": "#1B2631",
+          "950": "#141C24",
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
         },
         footer: "#D1E2EB",
         textGrey: "#555555",
         textLowerGrey: "#ADADAD",
         bgGrey: "#F5F5F5",
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        chart: {
+          "1": "hsl(var(--chart-1))",
+          "2": "hsl(var(--chart-2))",
+          "3": "hsl(var(--chart-3))",
+          "4": "hsl(var(--chart-4))",
+          "5": "hsl(var(--chart-5))",
+        },
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
       },
     },
   },
-  plugins: [],
+  plugins: [tailwindcssAnimate],
 };
 export default config;
