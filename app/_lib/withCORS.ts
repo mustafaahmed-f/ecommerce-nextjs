c70f5f6f2a0b7614a149c406da7c81cd7b7a9e5b
Mustafa Ahmed <mostafafikry97@gmail.com>
2025-05-27 18:40:13 +0300

    - [ Modify ] => moved the webhook api to pages instead of app

--- app/_lib/withCORS.ts
+++ app/_lib/withCORS.ts
@@ -3,6 +3,7 @@ import { NextResponse } from "next/server";
 export const withCORS = (response: NextResponse, origin: string | null) => {
   if (origin && origin.startsWith("https://ecommerce-nextjs-by-mustafa")) {
     response.headers.set("Access-Control-Allow-Origin", origin);
+    response.headers.set("Access-Control-Allow-Credentials", "true");
   }
   response.headers.set("Access-Control-Allow-Methods", "POST, OPTIONS");
   response.headers.set(
